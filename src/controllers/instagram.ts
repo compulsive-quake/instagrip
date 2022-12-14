eb21dc2601964fa6e628c185e7e8d86dc547da07
Richard warren <richard.w@phonecheck.com>
2022-12-13 18:02:50 -0800

    added cache methods

--- src/controllers/instagram.ts
+++ src/controllers/instagram.ts
@@ -1,12 +1,8 @@
 import { IgApiClient } from 'instagram-private-api';
-import { red } from './queue';
+import * as queue from './queue';
 
-
-async function cookieSave(data: object) {
-  await red.call("JSON.SET", "doc", "$", '{"f1": {"a":1}, "f2":{"a":2}}');
-  // here you would save it to a file/database etc.
-  // you could save it to a file: writeFile(path, JSON.stringify(data))
-  return data;
+async function cookieSave(username: string, data: object) {
+  await queue.write('instagram:cookies', username, data);
 }
 
 function cookieExists() {
@@ -19,20 +15,29 @@ function cookieLoad() {
   return '';
 }
 
-(async () => {
+async function getAccounts() {
+  const accounts = await queue.read('accounts')
+}
+
+async function login(username: string, password: string, proxyUrl?: string) {
+
+
   const ig = new IgApiClient();
-  ig.state.generateDevice();
+  ig.state.generateDevice(username);
+  if (proxyUrl) {
+    ig.state.proxyUrl = proxyUrl;
+  }
 
   ig.request.end$.subscribe(async () => {
     const serialized = await ig.state.serialize();
-    delete serialized.constants; // this deletes the version info, so you'll always use the version provided by the library
-    await cookieSave(serialized);
+    delete serialized.constants;
+    await cookieSave(username, serialized);
   });
 
-  if (cookieExists()) {
-    await ig.state.deserialize(cookieExists());
-  }
-  // This call will provoke request.end$ stream
-  await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-  // Most of the time you don't have to login after loading the state
-})();
+  await ig.account.login(username, password);
+
+}
+
+async function init() {
+
+}
--- src/controllers/queue.ts
+++ src/controllers/queue.ts
@@ -5,3 +5,28 @@ import * as log from './log';
 export const red = new IORedis(redis);
 
 red.on('error', log.error);
+
+export async function write(name: string, id: string, data: unknown) {
+  const path = `${redis.prefix}:${name}:${id}`;
+  let stringData: string;
+
+  try {
+    stringData = JSON.stringify(data);
+  } catch (e) {
+    throw new Error('failed to stringify data object');
+  }
+
+  await red.call("JSON.SET", path, "$", stringData);
+}
+
+export async function read(name: string, id: string) {
+  const path = `${redis.prefix}:${name}:${id}`;
+
+  return red.call("JSON.GET", path, "$");
+}
+
+export async function all(name: string) {
+  const path = `${redis.prefix}:${name}`;
+
+  return red.call("JSON.GET", path, "$");
+}
